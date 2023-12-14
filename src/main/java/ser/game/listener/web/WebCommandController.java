5f708a046f5360a35ecc7b2cb067ceec14ea300c
admin <sapozhnikoff1996@gmail.com>
2023-12-14 18:53:14 +0700

    thymeleaf

--- src/main/java/ser/game/listener/web/WebCommandController.java
+++ src/main/java/ser/game/listener/web/WebCommandController.java
@@ -5,7 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @AllArgsConstructor
 @RequestMapping(path = "coliseum/game")
@@ -23,9 +26,9 @@ public class WebCommandController {
         return ResponseEntity.ok().body("Please, use get request");
     }
 
-    @GetMapping
-    public ResponseEntity<String> getRequest() {
-        log.info("WoW i intercepted the GET request");
-        return ResponseEntity.ok().body("Please, use POST request");
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
     }
 }
